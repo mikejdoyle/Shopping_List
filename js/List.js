73698769114c2660568bce282b06924d73f072df
Mike D <mike@buzzvertical.com>
2013-11-13 13:30:25 -0600

    Added Angular JS functions and images

--- js/List.js
+++ js/List.js
@@ -1,6 +1,14 @@
 //file:///Users/michaeldoyle/coding_projects/shopping_list/index.html
 function slctrl ($scope){
-  $scope.itemslist = [{text:"bacon",bought:true}];
-  $scope.additem = function(){$scope.itemslist.push({text:$scope.itemtext,bought:false});$scope.itemtext = "";}
+  $scope.itemlist = [{text:"bacon",bought:true}];
+  $scope.additem = function(){$scope.itemlist.push({text:$scope.itemtext,bought:false});$scope.itemtext = "";}
+  $scope.strike = function(item){item.bought?(item.bought=false):(item.bought=true);}
+  $scope.clear = function(){
+          var oldList = $scope.itemlist;
+          $scope.itemlist = [];
+          angular.forEach (oldList, function (item){
+            if(!item.bought) $scope.itemlist.push(item);
+          });
+  };
 }//end of crtl
 
